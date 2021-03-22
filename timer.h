2b9ff9d959b2a76f59e7d329aded607a7ec6f776
Ivan Bykov <40389609+llunohodl@users.noreply.github.com>
2021-03-22 19:04:33 +0500

    Create timer.h

---
+++ timer.h
@@ -0,0 +1,23 @@
+#include "freertos/FreeRTOS.h"
+
+static TimerHandle_t timeout_timer=NULL;
+
+void timer_func(TimerHandle_t xTimer){
+   /* Do something */ 
+}
+
+/* Create timeout timer */
+timer = xTimerCreate("timer",15000/portTICK_PERIOD_MS,pdFALSE,NULL,timer_func);
+
+/* Create periodically call timer */
+timer = xTimerCreate("timer",15000/portTICK_PERIOD_MS,pdTRUE,NULL,timer_func);
+
+/* Start timer */
+xTimerStart(timer,0);
+/* Stop timer */
+xTimerStop(timer,0);
+
+/* Reset timer */
+xTimerReset(timer,0);
+/* Change timer period */
+xTimerChangePeriod(timer,5000/portTICK_PERIOD_MS,0);
